a1612f6ae39b6d0cf6df17867694a6e79b966cd5
Hannes Kristján Hannesson <hanneskr4@gmail.com>
2018-11-02 17:21:24 +0000

    redid episodelistitem

--- my-tv-list/src/Components/EpisodeListItem/EpisodeListItem.jsx
+++ my-tv-list/src/Components/EpisodeListItem/EpisodeListItem.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import '../../App.css';
-import { ListGroupItem } from "react-bootstrap";
+import { Panel } from "react-bootstrap";
 
 class EpisodeListItem extends Component {
     render() {
         return (
-            <ListGroupItem header={this.props.show}>
-                Season {this.props.season}, Episode {this.props.episode}, "{this.props.episodeName}"
-            </ListGroupItem>
+            <Panel eventKey={this.props.episodeName}>
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        {this.props.show} Season {this.props.season}, Episode {this.props.episode}: {this.props.episodeName}
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                    <div>
+                        "{this.props.description}"
+                    </div>
+                    <div>
+                        Episode premiered on {this.props.airdate}
+                    </div>
+                </Panel.Body>
+            </Panel>
         );
     }
 }
